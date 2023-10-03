ee0e4f3426f082ed2d88487f87bb0d3aed85ac34
pretox08 <pedropaulocarvalho08@gmail.com>
2023-10-03 14:39:34 -0300

    n ta aparecendo o site pra mim

--- src/App.js
+++ src/App.js
@@ -3,8 +3,43 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>OcaMarket</h1>
+    <div className="pagina-mercado">
+      <header>
+        <img src='./assets/images/OcaMarketlogo.png'></img>
+      </header>
+
+      <main>
+        <h1>ADICIONAR PRODUTO</h1>
+
+        <div className='tela-cadastro'>
+          <div>
+            <h5>Nome do Produto:</h5>
+            <input></input>
+          </div>
+
+          <div>
+            <h5>Tipo do Produto</h5>
+            <input></input>
+          </div>
+
+          <div>
+            <h5>Preço</h5>
+          </div>
+
+          <div>
+            <h5>Estoque</h5>
+            <input></input>
+          </div>
+
+          <div>
+            <h5>Validade</h5>
+            <input type='data'></input>
+          </div>
+
+
+          <button>ADICIONAR</button>
+        </div>
+      </main>
     </div>
   );
 }
